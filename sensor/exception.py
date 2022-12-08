93af1497fca18cef02a436bff6d562833fc5b89b
bhanu2905 <dhiman.bhanu2329@gmail.com>
2022-12-08 17:25:19 +0530

    updated

--- sensor/exception.py
+++ sensor/exception.py
@@ -9,9 +9,15 @@ def error_message_details(error, error_details: sys):
         file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
-    
+
 
 
 class SensorException(Exception):
 
-def __init__(self,error_message, error_details:sys):
+    def __init__(self,error_message, error_detail:sys):
+        self,error_message = error_message_detail(
+            error_message, error_details= error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
